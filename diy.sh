27a9094b2c94fcb8589f5954cc0a9a447f5d27b2
lcymu <50705646+lcymu@users.noreply.github.com>
2020-02-09 15:55:29 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,4 +9,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/openwrt-develop/luci-app-koolproxy.git package/lean/luci-app-koolproxy
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+rm -rf package/lean/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
