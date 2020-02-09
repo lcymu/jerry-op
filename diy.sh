ab65cee13d25d25b88de19010d38734aa1789e7e
lcymu <50705646+lcymu@users.noreply.github.com>
2020-02-09 15:35:36 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/openwrt-develop/luci-app-koolproxy.git package/lean/luci-app-koolproxy
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
