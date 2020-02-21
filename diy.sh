dbe276f2e23d853414be7bf7fc57451c6a6d43d1
lcymu <50705646+lcymu@users.noreply.github.com>
2020-02-21 11:11:37 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -11,3 +11,4 @@ git clone https://github.com/openwrt-develop/luci-app-koolproxy.git package/lean
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
